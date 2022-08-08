ba7413ace5b519c9465373dc3a4347b6f09a88ca
norrkin <norrkin@icloud.com>
2022-08-08 17:10:54 +0100

    updating apps

--- install-apps-zsh.sh
+++ install-apps-zsh.sh
@@ -6,6 +6,7 @@ brew install --cask google-chrome
 brew install --cask parallels
 brew install --cask postman
 brew install --cask rectangle
+brew install --cask signal
 brew install --cask spotify
 brew install --cask visual-studio-code
 brew install --cask vlc
